17f23681789b8334256045d45693ffbb65bc6970
Rituja  Prabhakar Chavan <147752228+chavanrituja@users.noreply.github.com>
2026-07-25 22:43:04 +0530

    Add: split-array-largest-sum - submission-0

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canSplit(vector<int>& nums, int k, long long maxSum) {
+        int count = 1;
+        long long sum = 0;
+
+        for (int num : nums) {
+            if (sum + num <= maxSum) {
+                sum += num;
+            } else {
+                count++;
+                sum = num;
+                if (count > k) return false;
+            }
+        }
+
+        return true;
+    }
+
+    int splitArray(vector<int>& nums, int k) {
+        long long low = *max_element(nums.begin(), nums.end());
+        long long high = 0;
+
+        for (int num : nums)
+            high += num;
+
+        while (low < high) {
+            long long mid = low + (high - low) / 2;
+
+            if (canSplit(nums, k, mid))
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+};
