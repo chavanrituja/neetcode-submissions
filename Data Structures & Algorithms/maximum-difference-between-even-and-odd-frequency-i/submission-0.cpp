138232cd5dee122ccb63e522f9c1931d0af94b53
Rituja  Prabhakar Chavan <147752228+chavanrituja@users.noreply.github.com>
2026-09-04 22:48:34 +0530

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-0

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-0.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxDifference(string s) {
+        vector<int> freq(26, 0);
+
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
+
+        int maxOdd = INT_MIN;
+        int minEven = INT_MAX;
+
+        for (int f : freq) {
+            if (f == 0) continue;
+
+            if (f % 2 == 1)
+                maxOdd = max(maxOdd, f);
+            else
+                minEven = min(minEven, f);
+        }
+
+        return maxOdd - minEven;
+    }
+};
